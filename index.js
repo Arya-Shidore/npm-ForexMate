96e9b47b8aa04c0de3f6a6cba925586b8ce859ae
Arya-Shidore <aryashidore2002@gmail.com>
2024-08-30 22:58:42 +0530

    edits in index.js file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 
 const freecurrencyapi = new Freecurrencyapi('fca_live_OuF0BNMBpfk39l6XIVJVpLvdqRtEvQk8Trdnevpf');
 
-export async function convertCurrency(fromCurrency, toCurrency, units) {
+export async function forexExchange(fromCurrency, toCurrency, units) {
     const res = await freecurrencyapi.latest({
         base_currency: fromCurrency,
         currencies: toCurrency
